7337e22d95c1753b8973119146db27c8cff22eb2
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2023-03-03 01:36:16 +0700

    feat: add raw predict

--- main.py
+++ main.py
@@ -21,6 +21,19 @@ def color_predict():
 
     return jsonify({'msg': 'success', 'predicted': result})
 
+@app.route("/raw_color", methods=["POST"])
+def raw_color_predict():
+    if request.method != "POST":
+        return jsonify({'msg': 'notfound', 'predicted': None})
+
+    if request.files.get("image"):
+        im_file = request.files["image"]
+        im_bytes = im_file.read()
+        file_bytes = np.asarray(bytearray(io.BytesIO(im_bytes).read()), dtype=np.uint8)
+        img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        result = predict.raw_predict(img)
+
+    return jsonify({'msg': 'success', 'predicted': result})
 @app.route("/")
 def ping():
     return "Hello, This is service for Car Color Classification"
--- predict.py
+++ predict.py
@@ -1,11 +1,17 @@
-from preprocess import normalize
+from preprocess import normalize, raw_palette
 import pickle
 import cv2
 
 NORM_KNN_MODEL = pickle.load(open('normKNN.sav', 'rb'))
+RAW_RF_MODEL = pickle.load(open('rawModel.sav', 'rb'))
 
 def knn_predict (image: cv2.Mat):
     lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
     norm = normalize(lab)
     result = NORM_KNN_MODEL.predict([norm])
+    return result[0]
+
+def raw_predict (image: cv2.Mat):
+    par = raw_palette(image)
+    result = RAW_RF_MODEL.predict([par])
     return result[0]
